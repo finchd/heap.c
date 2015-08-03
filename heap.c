4025fee5bf0fe4598623ca0505620df7d9456e75
brianbreniser <brianbreniser@gmail.com>
2015-08-03 15:52:50 -0700

    bug fixing

--- heap.c
+++ heap.c
@@ -1,17 +1,15 @@
-int lchild(int x)
+#include <math.h>
+
+int lchild(int x){
   return 2 * x + 1;
 }
 
-int rchild(int x)
+int rchild(int x){
   return 2 * x + 2;
 }
 
-int parent(int x)
-  return x / 2;
-}
-
-int upHeap(int x, struct minHeap *ourHeap){ //TODO build upHeap
-  return 0;
+int parent(int x){
+  return floor(x / 2);
 }
 
 struct minHeap {
@@ -19,6 +17,10 @@ struct minHeap {
   int heapSize = 2;
   int data[heapSize];
 };
+
+int upHeap(int x, struct minHeap *ourHeap){ //TODO build upHeap
+  return 0;
+}
 
 int main (int argc, char *argv[]){
   struct minHeap testHeap;
@@ -28,7 +30,7 @@ int main (int argc, char *argv[]){
   exit(0);
 }
 
-int add(int value, struct minHeap *h){
+int add(int value, minHeap *h){
   if( heapItems == 0) { // We're empty
     minHeap[1] = value;
     return 0;
