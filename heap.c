89fd0c9fb46bdf4fd025156c7ee455252ff71930
Donovan Finch <finchd@server.fake>
2015-08-03 16:47:05 -0700

    Refactor array operations into main

--- heap.c
+++ heap.c
@@ -1,8 +1,5 @@
 #include <math.h>
-
-int heapItems = 0;
-int heapSize = 16;
-int minHeap[heapSize];
+#include <stdlib.h>
 
 int lchild(int x){
   return 2 * x + 1;
@@ -16,46 +13,49 @@ int parent(int x){
   return floor(x / 2);
 }
 
-int upHeap(int x){ //TODO build upHeap
-  if(x == 1) {
-    return 0; // if we are at the root, stop
-  }
-  
-  if(minHeap[parent(x)] =< minHeap[x]) {
-    return 0; //already satisfy heap-invariant for min-heap
-  }
-    
-  // swap x and parent(x)
-  int temp = minHeap[x];
-  minHeap[x] = minHeap[parent(x)];
-  minHeap[parent(x)] = temp;
-  
-  upHeap(parent(x));
-
-  return 0;
-}
-
 int main (int argc, char *argv[]){
+  int heapItems = 0;
+  int heapSize = 16;
+  int minHeap[heapSize];
+  
+  int upHeap(int x){ //TODO build upHeap
+    if(x == 1) {
+      return 0; // if we are at the root, stop
+    }
+    
+    if(minHeap[parent(x)] =< minHeap[x]) {
+      return 0; //already satisfy heap-invariant for min-heap
+    }
+      
+    // swap x and parent(x)
+    int temp = minHeap[x];
+    minHeap[x] = minHeap[parent(x)];
+    minHeap[parent(x)] = temp;
+    
+    upHeap(parent(x));
+  
+    return 0;
+  }
+  
+  int add(int value){
+    if( heapItems == 0) { // We're empty
+      minHeap[1] = value;
+      ++heapItems;
+      return 0;
+    }
+    if( (heapItems + 1) == heapSize ) { // We're full
+      //TODO grow array
+    }
+  
+    // default case, add to end and upheap
+    ++heapItems;
+    minHeap[heapItems] = value;
+    upHeap(heapItems);
+    return 0;
+  }
   
   add(33);
   add(27);
 
   exit(0);
-}
-
-int add(int value){
-  if( heapItems == 0) { // We're empty
-    minHeap[1] = value;
-    ++heapItems;
-    return 0;
-  }
-  if( (heapItems + 1) == heapSize ) { // We're full
-    //TODO grow array
-  }
-
-  // default case, add to end and upheap
-  ++heapItems;
-  minHeap[heapItems] = value;
-  upHeap(heapItems);
-  return 0;
 }
