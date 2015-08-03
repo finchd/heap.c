4188a85995ed9c2cafc18717c3758402333cb88a
brianbreniser <brianbreniser@gmail.com>
2015-08-03 15:48:18 -0700

    re copied

--- heap.c
+++ heap.c
@@ -1,57 +1,46 @@
-int lchild(int x){
-    return 2 * x + 1;
+int lchild(int x)
+  return 2 * x + 1;
 }
 
-int rchild(int x) {
-    return 2 * x + 2;
+int rchild(int x)
+  return 2 * x + 2;
 }
 
-int parent(int x){
-    return x / 2;
+int parent(int x)
+  return x / 2;
 }
 
-int upHeap(int x){ //TODO build upHeap
-    return 0;
-}
-
-// int add(int value){
-//  if( heapItems == 0) { // We're empty
-//    minHeap[1] = value;
-//    return 0;
-//  }
-//  if( (heapItems + 1) == heapSize ) { // We're full
-    //TODO
-    //    return 0;
-//  }
-  // default case, add to end and upheap
-//  ++heapItems;
-//  minHeap[heapItems] = value;
-//  upHeap(heapItems);
-//  return 0;
-// }
-
-int main (int argc, char *argv[]){
-  int heapItems = 0;
-  int heapSize = 2;
-  int minHeap[heapSize];
-
-  int add(int value){
-    if( heapItems == 0) { // We're empty
-      minHeap[1] = value;
-      return 0;
-    }
-    if( (heapItems + 1) == heapSize ) { // We're full
-      //TODO
-      return 0;
-    }
-
-    // default case, add to end and upheap
-    ++heapItems;
-    minHeap[heapItems] = value;
-    upHeap(heapItems);
-    return 0;
-  }
-
+int upHeap(int x, struct minHeap *ourHeap){ //TODO build upHeap
   return 0;
 }
 
+struct minHeap {
+  int heapItems = 0;
+  int heapSize = 2;
+  int data[heapSize];
+};
+
+int main (int argc, char *argv[]){
+  struct minHeap testHeap;
+  
+  add(33, &testHeap);
+  
+  exit(0);
+}
+
+int add(int value, struct minHeap *h){
+  if( heapItems == 0) { // We're empty
+    minHeap[1] = value;
+    return 0;
+  }
+  if( (heapItems + 1) == heapSize ) { // We're full
+    //TODO
+    return 0;
+  }
+  
+  // default case, add to end and upheap
+  ++heapItems;
+  minHeap[heapItems] = value;
+  upHeap(heapItems);
+  return 0;
+}
